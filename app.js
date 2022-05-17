16e47088bb5a1c08e769e2d66ab2b3261ae30d55
Cristiano Tobias <cristiano.tobias40@gmail.com>
2022-05-17 17:27:49 -0300

    Tetris
    update

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10
+
+//The tetrominoes
+const lTetromino = [
+    []
+]
+
+
+
+
+})
